16e5ea16001d75f044c37ba67905558b697f1918
geraldynboholstt@gmail.com <geraldynboholstt@gmail.com>
2025-11-28 01:28:12 +0800

    Add Capacitor Filesystem and Share plugins; implement file download and sharing functionality

--- android/app/src/main/java/com/canemap/app/MainActivity.java
+++ android/app/src/main/java/com/canemap/app/MainActivity.java
@@ -3,3 +3,31 @@ package com.canemap.app;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {}
+
+import android.Manifest;
+import android.os.Build;
+
+@Override
+public void onStart() {
+    super.onStart();
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+        requestPermissions(new String[]{
+                Manifest.permission.CAMERA
+        }, 1001);
+    }
+}
+
+import android.webkit.WebChromeClient;
+
+@Override
+public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    this.bridge.getWebView().setWebChromeClient(new WebChromeClient(){
+        @Override
+        public void onPermissionRequest(final android.webkit.PermissionRequest request) {
+            runOnUiThread(() -> request.grant(request.getResources()));
+        }
+    });
+}
